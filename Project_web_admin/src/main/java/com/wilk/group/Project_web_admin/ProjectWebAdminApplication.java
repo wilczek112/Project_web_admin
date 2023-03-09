bfc10de39f1b454adc14b08a575e1e59514b1af1
Admin <wilk.o@o2.pl>
2023-03-09 11:08:17 +0100

    first commit

---
+++ Project_web_admin/src/main/java/com/wilk/group/Project_web_admin/ProjectWebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.wilk.group.Project_web_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebAdminApplication.class, args);
+	}
+
+}
