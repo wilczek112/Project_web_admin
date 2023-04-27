98025da6a88721f3021f393b0ce8a64b253493b9
malinka32 <44645546+malinka32@users.noreply.github.com>
2023-04-27 09:53:48 +0200

    Add files via upload

---
+++ Project_web_admin/project_web_admin - strona/lobby.php
@@ -0,0 +1,12 @@
+
+<html lang="pl">
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<title>Project_Web_Admin lobby</title>
+<meta name="viewport" content="width=devide-width, initial-scale=1.0">
+</head>
+<body>
+<h1> Jesteś zalogowany</h1>
+</body>
+</html>
