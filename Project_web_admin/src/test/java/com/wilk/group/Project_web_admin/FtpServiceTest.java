aaad75e49246c066247848c6652b02a225cebe7f
Admin <wilk.o@o2.pl>
2023-05-20 22:02:04 +0200

    ftp faktycznie działa!

---
+++ Project_web_admin/src/test/java/com/wilk/group/Project_web_admin/FtpServiceTest.java
@@ -0,0 +1,32 @@
+package com.wilk.group.Project_web_admin;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FtpServiceTest {
+
+    @Autowired
+    private FtpService ftpService;
+
+    @Test
+    public void test() throws Exception {
+        long currentTimeMillis = System.currentTimeMillis();
+        String directoryPath = String.format("/G/create-directory-test-%d", currentTimeMillis);
+        String path = String.format("/G/test-upload-file-%d.jpg", currentTimeMillis);
+        String newPath = String.format("/G/test-rename-file-%d.jpg", currentTimeMillis);
+        FTPClient ftpClient = ftpService.loginFtp("192.168.0.1", 21, "admin", "null");
+        ftpService.printTreeDir("/G/", ftpClient);
+        ftpService.printTreeFiles("/G/", ftpClient);
+        ftpService.createDirectory(directoryPath, ftpClient);
+        ftpService.uploadFile("C:\\test.jpg", path, ftpClient);
+        ftpService.renameFile(path, newPath, ftpClient);
+        byte[] downloadFile = ftpService.downloadFile(newPath, ftpClient);
+        //System.out.println("Downloaded file : " + new String(downloadFile));
+        ftpService.deleteFile(newPath, ftpClient);
+        ftpService.deleteDirectory(directoryPath, ftpClient);
+        ftpClient.disconnect();
+    }
+}
