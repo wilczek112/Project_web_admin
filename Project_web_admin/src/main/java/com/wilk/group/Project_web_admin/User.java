36ff0618867bec8239555204badabea7998f87e0
Jakub_W <walczak0105@gmail.com>
2023-05-24 11:37:20 +0200

    formularz i błędy prawie działa

--- Project_web_admin/src/main/java/com/wilk/group/Project_web_admin/User.java
+++ Project_web_admin/src/main/java/com/wilk/group/Project_web_admin/User.java
@@ -2,6 +2,8 @@ package com.wilk.group.Project_web_admin;
 
 import jakarta.persistence.*;
 import lombok.*;
+import jakarta.validation.*;
+
 
     @Getter
     @Setter
@@ -13,14 +15,22 @@ import lombok.*;
         private Long id;
 
         @Column(name = "login", nullable = false, unique = true, length = 32)
+        @jakarta.validation.constraints.Size(min=5, max = 15,  message = "Login musi zajmować od 5 do 15 znaków")
+        @jakarta.validation.constraints.NotEmpty(message = "Musisz wpisac swój login")
         private String login;
 
         @Column(name = "password", nullable = false, length = 64)
+        @jakarta.validation.constraints.Pattern(regexp = "((?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%]).{6,20})",
+                message = "Hasło jest błędne")
+        @jakarta.validation.constraints.NotEmpty(message = "Hasło jest wymagane")
         private String password;
 
         @Column(name = "email", nullable = false, unique = true, length = 32)
+        @jakarta.validation.constraints.NotEmpty(message = "Musisz podać swój email")
+        @jakarta.validation.constraints.Email(message = "Email został źle wprowadzony")
         private String email;
 
         @Column(name = "privileges", nullable = false, length = 8)
+        @jakarta.validation.constraints.NotEmpty
         private String privileges;
     }
